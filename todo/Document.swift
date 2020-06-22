35b78379fcb1015ebac1bcc26d0d5406ed2fdabc
Tyler Hall <rth@tyler.io>
2020-06-21 22:20:15 -0500

    Adding .gitignore

---
+++ todo/Document.swift
@@ -0,0 +1,9 @@
+//
+//  Document.swift
+//  todo
+//
+//  Created by Tyler Hall on 6/21/20.
+//  Copyright © 2020 Tyler Hall. All rights reserved.
+//
+
+import Foundation
---
+++ todo/Ticket.swift
@@ -0,0 +1,9 @@
+//
+//  Ticket.swift
+//  todo
+//
+//  Created by Tyler Hall on 6/21/20.
+//  Copyright © 2020 Tyler Hall. All rights reserved.
+//
+
+import Foundation
